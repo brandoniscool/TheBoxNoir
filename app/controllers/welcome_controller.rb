9e546203bab1f24d3f379217b905fe44f8fd8bed
Brandon Thornton <brandon@balloonduck.com>
2013-10-11 21:29:20 -0700

    add waitlist field

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+		@waitlist = Waitlist.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Topshelf::Application.routes.draw do
 	root to: "welcome#index"
 	
 	resources :users
+	
+	resources :waitlists
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
