70e657283b76d03f8fbf308426c624dd1db41790
Vivian Xue <vivian@balloonduck.com>
2013-10-19 15:02:36 -0700

    modify asset sync

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,9 +26,6 @@ TheBoxNoir::Application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
   # Generate digests for assets URLs.
   config.assets.digest = true
 
@@ -59,7 +56,13 @@ TheBoxNoir::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+    config.assets.precompile += %w( search.js )
+    
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
+
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
